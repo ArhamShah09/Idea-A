138e5d920c94b2f78feda7ebdfdad2f621c56a75
ArhamShah09 <65812990+ArhamShah09@users.noreply.github.com>
2020-08-24 16:30:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var canvas, sprite;
+
+function preload() {
+
+}
+
+function setup() {
+    
+
+}
+
+function draw() {
+    canvas = createCanvas(displayWidth,displayHeight);
+
+    sprite = createSprite(400,400,100,100);
+    
+
+}
